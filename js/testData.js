71f579fed5652d5c55316c3039163779c828074a
unknown <xiaowenz@SJCLXIAOWENZ00.citrite.net>
2014-10-07 21:48:57 -0700

    add class pk to testData

--- js/testData.js
+++ js/testData.js
@@ -3,9 +3,10 @@ var testDataStore = {
 
 var TestData = function(data){
 	this.pk = data[0];
-	this.name = data[1];
-	this.isRead = data[2];
-	this.isExam = data[3];
+	this.clazz = data[1];
+	this.name = data[2];
+	this.isRead = data[3];
+	this.isExam = data[4];
 
 	this.setRead = function() {
 		this.isRead = true;
@@ -18,17 +19,17 @@ testDataStore.setUIPage = function(page) {
 };
 
 var testAssignmentDataList = [
-	new TestData([3, '1.1 Assignment', true, false]),
-	new TestData([4, '1.2 Assignment', true, false]),
-	new TestData([5, '2.1 Assignment', true, false]),
-	new TestData([6, '2.2 Assignment', true, false]),
-	new TestData([7, '2.3 Assignment', true, false]),
-	new TestData([8, '3.1 Assignment', true, false])
+	new TestData([3, 1, '1.1 Assignment', true, false]),
+	new TestData([4, 1, '1.2 Assignment', true, false]),
+	new TestData([5, 2, '2.1 Assignment', true, false]),
+	new TestData([6, 3, '2.2 Assignment', true, false]),
+	new TestData([7, 2, '2.3 Assignment', true, false]),
+	new TestData([8, 1, '3.1 Assignment', true, false])
 ];
 
 var testExamDataList = [
-	new TestData([1, '2014 Final Test', false, true]),
-	new TestData([2, '2014 Middle Test', true, true])
+	new TestData([1, 3, '2014 Final Test', false, true]),
+	new TestData([2, 1, '2014 Middle Test', true, true])
 ];
 
 testDataStore.getAssignmentDataList = function() {
