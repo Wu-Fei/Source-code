1d4ba249df4ca651c4be14ef182fc2a29e3253de
unknown <xiaowenz@SJCLXIAOWENZ00.citrite.net>
2014-10-28 16:52:00 -0700

    add tinyeditor for rich text editing in admin

--- admin/js/createQaPage.js
+++ admin/js/createQaPage.js
@@ -6,9 +6,33 @@ var createQaPage = function() {
     var txtAnswer = form.find('textarea[name=answer]');
     var txtSearch = $('#condition');
 
+	var editor = new TINY.editor.edit('editor', {
+		id: 'tinyeditor',
+		width: 584,
+		height: 175,
+		cssclass: 'tinyeditor',
+		controlclass: 'tinyeditor-control',
+		rowclass: 'tinyeditor-header',
+		dividerclass: 'tinyeditor-divider',
+		controls: ['bold', 'italic', 'underline', 'strikethrough', '|', 'subscript', 'superscript', '|',
+			'orderedlist', 'unorderedlist', '|', 'outdent', 'indent', '|', 'leftalign',
+			'centeralign', 'rightalign', 'blockjustify', '|', 'unformat', '|', 'undo', 'redo', 'n',
+			'font', 'size', 'style', '|', 'image', 'hr', 'link', 'unlink', '|', 'print'],
+		footer: true,
+		fonts: ['Verdana','Arial','Georgia','Trebuchet MS'],
+		xhtml: true,
+		cssfile: 'custom.css',
+		bodyid: 'editor',
+		footerclass: 'tinyeditor-footer',
+		toggle: {text: 'source', activetext: 'wysiwyg', cssclass: 'toggle'},
+		resize: {cssclass: 'resize'}
+	});
+
     $('#saveanswer').on('click', function () {
         //realStart();
         //return;
+
+		editor.post();
 
         var question = $.trim(txtQuestion.val());
         var answer = $.trim(txtAnswer.val());
